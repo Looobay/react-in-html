689fb8e6daa50b70cb6df1bd6da625fe151a0926
Looobay <69517054+Looobay@users.noreply.github.com>
2024-09-08 17:19:25 +0200

    now count!

--- src/app.jsx
+++ src/app.jsx
@@ -1,17 +1,43 @@
-function Button({ text, count, onClick }) {
+const { useState } = React;
+
+function Button({ text, onClick }) {
     return (
         <div>
             <button onClick={onClick}>
-                {text} {count}
+                {text}
             </button>
         </div>
     );
+}
+
+function FunButton(){
+    let [number, setNumber] = useState(0);
+
+    function FunClick(){
+        setNumber(number + 1)
+    }
+
+    return(
+        <div>
+            <p>
+                {number}
+            </p>
+            <button onClick={FunClick}>
+                Funny lol
+            </button>
+        </div>
+    )
 }
 
 function App() {
     function classic(){
         alert("Hello React!")
     }
+
+    function counting(){
+        setCount(count + 1)
+    }
+
 
     return (
         <div className="App">
@@ -23,6 +49,7 @@ function App() {
                 <a className="App-link" href="https://github.com/Looobay/react-in-html" target="_blank" rel="noopener noreferrer">Github Page</a>
                 <a className="App-link" href="https://react.dev/learn" target="_blank" rel="noopener noreferrer">Learn React</a>
                 <Button className="App-button" text={'Classic React Button'} onClick={classic}></Button>
+                <FunButton></FunButton>
             </header>
         </div>
     );
